bcfbacebd83557ad5c21601cba56a8d1a80b506e
Emilia <emiliaprzybylek@tutamail.com>
2022-03-24 13:25:51 +0100

    js: script count bills added

--- js/script.js
+++ js/script.js
@@ -3,4 +3,20 @@
 const form = document.querySelector('form');
 const price = document.querySelector('#price');
 const tip = document.querySelector('#tip');
-const resultPlace = document.querySelector('.result');
+const resultPlaceTip = document.querySelector('.result-tip');
+const resultPlaceBill = document.querySelector('.result-bill');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const countTip = parseFloat(price.value) * (parseInt(tip.value) / 100);
+    
+    resultPlaceTip.textContent = countTip.toFixed(2);
+
+    const countBill = parseFloat(price.value) + countTip;
+    resultPlaceBill.textContent = countBill.toFixed(2);
+
+   
+
+});
