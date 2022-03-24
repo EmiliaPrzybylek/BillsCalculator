be272e20cede0bc5a15be02cfa61b52270268d3f
Emilia <emiliaprzybylek@tutamail.com>
2022-03-24 14:18:08 +0100

    js: count correct

--- js/script.js
+++ js/script.js
@@ -3,15 +3,28 @@
 const form = document.querySelector('form');
 const price = document.querySelector('#price');
 const tip = document.querySelector('#tip');
-const resultPlaceTip = document.querySelector('.result-tip');
 const resultPlaceBill = document.querySelector('.result-bill');
+const resultPlaceTip = document.querySelector('.result-tip');
+const errorPlacePrice = document.querySelector('.error-price')
+const errorPlaceTip = document.querySelector('.error-tip')
 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const delayResult = setTimeout(delaySum, 500)
+    if(price.value == '') {
+        errorPlacePrice.style.display = "block";
+        resultPlaceBill.style.display = 'none'
+    } if(tip.value == '') {
+        errorPlaceTip.style.display = "block";
+        resultPlaceTip.style.display = 'none'
+    } else {
 
+    }
+
+
+
+    const delayResult = setTimeout(delaySum, 500)
     function delaySum() {
         const countTip = parseFloat(price.value) * (parseInt(tip.value) / 100);
         resultPlaceTip.textContent = countTip.toFixed(2);
@@ -23,4 +36,7 @@ form.addEventListener('submit', (e) => {
     } 
 
 });
+
+
+
 
