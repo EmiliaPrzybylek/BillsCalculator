c3796636e9f3915f26cd8f56093da7ed1972e57d
Emilia <emiliaprzybylek@tutamail.com>
2022-03-24 10:55:44 +0100

    js: variables added

---
+++ js/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const form = document.querySelector('form');
+const price = document.querySelector('#price');
+const tip = document.querySelector('#tip');
+const resultPlace = document.querySelector('.result');
