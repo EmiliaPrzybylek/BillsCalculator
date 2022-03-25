661078178cfebbae6d39d977324343ed0b130277
Emilia <emiliaprzybylek@tutamail.com>
2022-03-25 11:51:33 +0100

    html & css: result-box added and js: script modified

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const price = document.querySelector('#price');
 const tip = document.querySelector('#tip');
 const resultPlaceBill = document.querySelector('.result-bill');
 const resultPlaceTip = document.querySelector('.result-tip');
-const resultText = document.querySelector('.resultText');
+const resultText = document.querySelector('.result-box');
 const errorPlacePrice = document.querySelector('.error-price')
 const errorPlaceTip = document.querySelector('.error-tip')
 
@@ -13,16 +13,20 @@ const errorPlaceTip = document.querySelector('.error-tip')
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    if(price.value == '') {
-        errorPlacePrice.style.display = "block";
-        resultPlaceBill.style.display = 'none'
-    } if(tip.value == '') {
-        errorPlaceTip.style.display = "block";
-        resultPlaceTip.style.display = 'none'
-    } 
+    
 
-    const delayResult = setTimeout(delaySum, 500)
+    const delayResult = setTimeout(delaySum, 400)
     function delaySum() {
+
+        if(price.value == '') {
+            errorPlacePrice.style.display = "block";
+            resultPlaceBill.style.display = 'none'
+        } if(tip.value == '') {
+            errorPlaceTip.style.display = "block";
+            resultPlaceTip.style.display = 'none'
+        } if(price.vaule != '' && tip.value != ''){
+            resultText.style.display = 'block'
+        }
 
         const countTip = parseFloat(price.value) * (parseInt(tip.value) / 100);
         resultPlaceTip.textContent = countTip.toFixed(2);
